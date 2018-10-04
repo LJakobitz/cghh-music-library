8c4ae444d185eb3b8e5c57e03325540456ddc2ae
LJakobitz <l.jakobitz@dev5310.com>
2018-10-04 16:25:49 +0200

    Add Route to RegisterUser

---
+++ client/src/components/Register.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>HUHU</p>
+</template>
+
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
