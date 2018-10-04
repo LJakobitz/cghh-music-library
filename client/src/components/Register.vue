e4538750d9705ef48ce7a178411edcf4c93989b4
LJakobitz <l.jakobitz@dev5310.com>
2018-10-04 17:32:00 +0200

    ADD inputs to register Site

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="Register">
-    <p>HUHU</p>
+    <p>REGISTER</p>
+    <div class="form-container">
+      <label for="email">EMAIL:</label>
+      <input id="email" type="email"/>
+      <br>
+      <label for="password">PASSWORD:</label>
+      <input id="password" type="password" />
+    </div>
   </div>
 </template>
 
@@ -14,3 +21,24 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .form-container{
+    text-align: left;
+    margin-left: 25%;
+    margin-right: 25%;
+
+    label{
+      margin-left:25%;
+      width:20%;
+      float:left;
+    }
+
+    input{
+      width:30%;
+      float:left;
+    }
+
+  }
+
+</style>
