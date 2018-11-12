c533d22ac0ee2f6d3a64fa3821f0e900527d9b32
LJakobitz <ljakobitz@dev5310.com>
2018-11-12 16:29:41 +0100

    add sequelization to branch

---
+++ server/src/routes.js
@@ -0,0 +1,9 @@
+const authenticationController = require('./controllers/authenticationController')
+const authenticationControllerPolicy = require('./policies/authenticationControllerPolicy')
+
+module.exports = (app) => {
+    app.post('/register', 
+    authenticationControllerPolicy.register,
+        authenticationController.register)
+}
+
